4eadf5ec7b7e753e0f58f0f87758084f55856613
Aechelon-Online <info@aechelon.online>
2021-11-16 09:36:43 -0600

    Added test plugin folder

---
+++ wp-content/plugins/our-first-unique-plugin/test.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    Plugin Name: Our Test Plugin
+    Description: A truly amazing plugin.
+    Version: 1.0
+    Author: Greg Luce
+    Author URI: https://aechelon.online/wordpress/author/greglucedev/
+*/
+
+?>
