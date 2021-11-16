92a7be4d5605ac02cfdab9b07f2347fc18caea3c
Aechelon-Online <info@aechelon.online>
2021-11-16 09:49:43 -0600

    Put correct user name

--- wp-content/plugins/our-first-unique-plugin/test.php
+++ wp-content/plugins/our-first-unique-plugin/test.php
@@ -5,5 +5,5 @@
   Description: A truly amazing plugin.
   Version: 1.0
   Author: Brad
-  Author URI: https://www.udemy.com/user/bradschiff/
+  Author URI: https://www.udemy.com/user/greg-luce-2/
 */
